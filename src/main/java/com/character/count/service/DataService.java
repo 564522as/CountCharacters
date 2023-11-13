a1ce094465368c1e8319ab254204ece7b7427469
ib <564522as@gmail.com>
2023-11-13 20:15:52 +0300

    Service changed

--- src/main/java/com/character/count/service/DataService.java
+++ src/main/java/com/character/count/service/DataService.java
@@ -1,2 +1,9 @@
-package com.character.count.service;public interface DataService {
+package com.character.count.service;
+
+import com.character.count.model.CharacterQuantityInString;
+
+import java.util.List;
+
+public interface DataService {
+    public List<CharacterQuantityInString> processData(String data);
 }
--- src/main/java/com/character/count/service/DataServiceImpl.java
+++ src/main/java/com/character/count/service/DataServiceImpl.java
@@ -1,8 +1,28 @@
 package com.character.count.service;
 
+import com.character.count.model.CharacterQuantityInString;
 import org.springframework.stereotype.Service;
 
-@Service
-public class DataServiceImpl {
+import java.util.*;
 
+@Service
+public class DataServiceImpl implements DataService{
+
+    @Override
+    public List<CharacterQuantityInString> processData(String data) {
+        List<CharacterQuantityInString> list = new ArrayList<>();
+        Set<Character> characters = changeToSet(data.toCharArray());
+        for (Character c: characters) {
+            list.add(new CharacterQuantityInString(data, c));
+        }
+        return list;
+    }
+
+    private Set<Character> changeToSet(char[] array) {
+        Set<Character> set = new HashSet<>();
+        for (Character c: array) {
+            set.add(c);
+        }
+        return set;
+    }
 }
